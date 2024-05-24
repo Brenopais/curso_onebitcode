9ebac257b842726545b5324464e2f6956d48ff03
Breno Pais <brenopais7@gmail.com>
2024-05-24 16:25:36 -0300

    estudo

---
+++ 05-Javascript/08-Switch/script.js
@@ -0,0 +1,15 @@
+const resultado = prompt("Escolha uma alternativa: \nA) \nB) \nC)");
+
+switch (resultado) {
+  case 'a':
+    alert("O resultado é 'A'");
+    break;
+  case 'b':
+    alert("O resultado é 'B'");
+    break;
+  case 'c':
+    alert("O resultado é 'C'");
+    break;
+  default:
+    alert("Finalizando...");
+}
---
+++ 05-Javascript/10-while/script.js
@@ -0,0 +1,8 @@
+let velocidade = 80
+
+while (velocidade > 0) {
+  alert("O carro está a " + velocidade + " km/h")
+  alert("diminuindo 20km/h")
+}
+
+alert("o carro parou")
