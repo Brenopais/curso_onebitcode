a313fdd556a5d3c8d41316b1ebdd7cf749161988
Breno Pais <brenopais7@gmail.com>
2024-08-20 16:16:42 -0300

    estudo diario

---
+++ 05-Javascript/16-Array/array.js
@@ -0,0 +1,10 @@
+const listadecompras = ["Açucar", false]
+console.log(listadecompras)
+
+listadecompras[0] = "arroz"
+listadecompras[1] = "Feijão"
+listadecompras[2] = 7
+listadecompras[5] = "Batata"
+console.log(listadecompras)
+
+console.log(listadecompras[4])
